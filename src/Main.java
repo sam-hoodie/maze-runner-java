c236e064f376ea4c65cb0ced0482bfe7a6e5a1a2
Sam G <112674910+sam-hoodie@users.noreply.github.com>
2024-05-13 22:37:52 -0400

    Added comments for user use

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //
+        // This is where the maze you want to solve is created
+        // 2 indicates the start of the maze, and 3 indicates the end of the maze. Only one of each  can be added into the maze.
+        // 1 indicates a wall. The maze must be surrounded by 1's completely.
+        // 0 indicates an empty space, and there must be a line of consecutive (non-diagonal) 0's for the program to find a solution
+        // An outline for an example maze has been provide below.
         int[][] maze = {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 2, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1},
@@ -18,7 +22,13 @@ public class Main {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
         Maze myMaze = new Maze();
-        myMaze.printMaze(maze, Maze.makePath(maze, new ArrayList<Cell>(), new Cell(1, 1), new ArrayList<Cell>()));
+        // Before running the code, change the third parameter of printMaze call to your starting cell position.
+        // The cell class uses an x/y format. The first portion of the cell is x, which starts at 0 and increases rightward.
+        // The second portion of the cell is y, which starts at 0 and increases downwards.
+        myMaze.printMaze(maze, Maze.makePath(maze, new ArrayList<>(), new Cell(1, 1), new ArrayList<>()));
+
+        // To run the code, simply click run on whichever application/website you are using to run the code.
+        // The result will appear in the console.
     }
 
 }
